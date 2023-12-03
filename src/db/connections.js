bd6c3f697f4247af0775a428c70e93b72e98c395
Suzair0318 <128297059+Suzair0318@users.noreply.github.com>
2023-12-03 07:47:48 +0500

    Update connections.js

--- src/db/connections.js
+++ src/db/connections.js
@@ -1,7 +1,7 @@
 const  mongoose = require('mongoose');
 
 
- const DatabaseConnection  = mongoose.connect( `mongodb+srv://Nazcollection_api:${process.env.PASSWORD}@cluster0.hpy8te0.mongodb.net/` 
+ const DatabaseConnection  = mongoose.connect( `mongodb+srv://Nazcollection_api:Nazcollection_api@cluster0.hpy8te0.mongodb.net/` 
   ).then(() => {
    console.log("Mongo_DB Connection");
 }).catch((e) =>{
